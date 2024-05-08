15eadce8c99fdccb87cb5c4b87afeb857aeb32dc
Kamila <ksalimzh@gmail.com>
2024-05-09 01:39:20 +0500

    table view correction

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -17,6 +17,7 @@ final class ImagesListViewController: UIViewController {
 
         tableView.rowHeight = 200
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+        tableView.isScrollEnabled = true
     }
 }
 
