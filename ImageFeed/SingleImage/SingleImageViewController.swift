a053bbf5009a0e1edb19e4f4f61f4b1cb9540736
Kamila <ksalimzh@gmail.com>
2024-05-18 04:31:34 +0500

    model view presentation

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -12,4 +12,7 @@ final class SingleImageViewController: UIViewController {
         super.viewDidLoad()
         imageView.image = image
     }
+    @IBAction private func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
 }
