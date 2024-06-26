47ceb3c4c5ddf454a3066db20907b58cba016f42
Kamila <ksalimzh@gmail.com>
2024-06-26 20:29:21 +0500

    ui tests

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -19,6 +19,7 @@ final class ProfileViewController: UIViewController & ProfileViewControllerProto
     private lazy var imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "ProfilePhoto")
+        logoutButton.accessibilityIdentifier = "logout button"
         imageView.layer.masksToBounds = true
         imageView.layer.cornerRadius = 35
         return imageView
