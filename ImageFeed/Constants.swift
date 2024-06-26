7fc3325a88264df87be06c53e7a33f11480a7775
Kamila <ksalimzh@gmail.com>
2024-06-26 18:50:18 +0500

    refactoring webviewviewcontroller and testing

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -10,3 +10,30 @@ enum Constants {
     static let profileURLString = "https://api.unsplash.com/me"
     static let baseURL = URL(string: "https://unsplash.com")!
 }
+
+struct AuthConfiguration {
+    let accessKey: String
+    let secretKey: String
+    let redirectURI: String
+    let accessScope: String
+    let defaultBaseURL: URL
+    let authURLString: String
+    
+    init(accessKey: String, secretKey: String, redirectURI: String, accessScope: String, authURLString: String, defaultBaseURL: URL) {
+        self.accessKey = accessKey
+        self.secretKey = secretKey
+        self.redirectURI = redirectURI
+        self.accessScope = accessScope
+        self.defaultBaseURL = defaultBaseURL
+        self.authURLString = authURLString
+    }
+    
+    static var standard: AuthConfiguration {
+        return AuthConfiguration(accessKey: Constants.accessKey,
+                                 secretKey: Constants.secretKey,
+                                 redirectURI: Constants.redirectURI,
+                                 accessScope: Constants.accessScope,
+                                 authURLString: Constants.unsplashAuthorizeURLString,
+                                 defaultBaseURL: Constants.defaultBaseURL)
+    }
+}
